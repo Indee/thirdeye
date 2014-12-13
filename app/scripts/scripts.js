afebcd34d55a7667b39a2d6b8ef82be530e31458
Jonjoe Whitfield <jonjoe@indee.io>
2014-12-13 23:34:57 +0000

    built cursor monitoring

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,1 +1,6 @@
-alert("this");
+document.addEventListener("mousemove", function(e){
+	console.log("X Is:" + e.clientX, "Y Is:" + e.clientY);
+	var x = e.clientX;
+	var y = e.clientY;
+	$("#pos").html("X Axis is: " + x + "<br>" + "Y Axis is: " + y);
+})
