09c6be9ed7a63df6aaa75d5aa6cdcd7f096f7901
Jonjoe Whitfield <jonjoe@indee.io>
2014-12-14 11:11:16 +0000

    further work

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -2,9 +2,7 @@ var ac = new AudioContext();
 
 // Inititalise ViewPort Dimensions 
 var h = window.innerHeight;
-console.log(h);
 var w = window.innerWidth;
-console.log(w);
 
 // Create Gain Nodes
 var gain_x = ac.createGain();
@@ -14,13 +12,18 @@ var gain_y = ac.createGain();
 var osc_x = ac.createOscillator();
 osc_x.connect(gain_x);
 gain_x.connect(ac.destination);
-osc_x.type = "square";
+osc_x.type = "sawtooth";
+gain_x.gain.value = 0;
+osc_x.frequency.value = 1000;
 osc_x.start();
 
 // Y Axis
 var osc_y = ac.createOscillator();
 osc_y.connect(gain_y);
+
 gain_y.connect(ac.destination);
+osc_y.frequency.value = 1000;
+gain_y.gain.value = 0;
 osc_y.type = "sawtooth";
 osc_y.start();
 
@@ -28,50 +31,41 @@ var freq_x;
 var freq_y;
 
 
+document.addEventListener("mousemove", function(e){	
+	gain_y.gain.value = 1;
+	gain_x.gain.value = 1;
 
-document.addEventListener("mousemove", function(e){
-
-	// console.log("X Is:" + e.clientX, "Y Is:" + e.clientY);
 	var x = w - e.clientX;
 	var y = h - e.clientY;
 
 	var xPercent = Math.round((x/w)*100);
 	var yPercent = Math.round((y/h)*100);
 
-	//console.log(h);
-	//console.log(w);
 
-	/*var x_perc = 200 + Math.floor(x/w*100)*10;
-	console.log(x_perc);
-	var y_perc = 200 + Math.floor(y/h*100)*10;
-	console.log(y_perc);*/
-
-
-
-	//$("#pos").html("X Axis is: " + x + "<br>" + "Y Axis is: " + y);
-	//$("#view").html("Viewport X: " + w + "<br>" + "Viewport Y: " + h);
-	$("#percent").html("X Percent: " + xPercent + "<br>" + "Y Percent: " + yPercent);
-
-	if(xPercent < 2){
-		freq_x = 0;
-	}else if(xPercent > 98){
-		freq_x = 400;
+	if(yPercent < 1.5){
+		freq_y = 0;
+	}else if(yPercent > 98.5){
+		freq_y = freq_y + 20;
 		console.log('400 wat');
 	}else{
-		freq_x = (xPercent/100)*2;
+		freq_y = yPercent/10+2;
+	}
+	osc_y.frequency.value = freq_y;
+
+	if(xPercent < 1.5){
+		freq_x = 0;
+	}else if(xPercent > 98.5){
+		freq_x = freq_x + 20;
+		console.log('400 wat');
+	}else{
+		freq_x = xPercent/10+2;
 	}
 
 	osc_x.frequency.value = freq_x;
 
-
-	if(yPercent < 2){
-		freq_y = 0;
-	}else if(yPercent > 98){
-		freq_y = 200;
-		console.log('400 wat');
-	}else{
-		freq_y = (yPercent/100)*2;
-	}
-	osc_y.frequency.value = freq_y;
-	
+	$("#pos-x").html(x);
+	$("#pos-y").html(y);
+	$("#view").html("Viewport X: " + w + "<br>" + "Viewport Y: " + h);
+	$("#percent").html("X Percent: " + xPercent + "<br>" + "Y Percent: " + yPercent);
+	$("#view").html("Center X: " + w / 2 + "<br>" + "Center Y: " + h / 2);
 })
