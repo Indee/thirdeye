7772ac37d6dfc08e3478e14377aa2eb2ef3c32e6
Jonjoe Whitfield <jonjoe@indee.io>
2014-12-14 02:48:30 +0000

    add audio guidence with web audio

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,7 +1,41 @@
+var ac = new AudioContext();
+
+// Inititalise ViewPort Dimensions 
+var h = window.innerHeight;
+console.log(h);
+var w = window.innerWidth;
+console.log(w);
+
+
+
+// X Axis 
+var osc_x = ac.createOscillator();
+osc_x.connect(ac.destination);
+osc_x.type = "sine";
+osc_x.start();
+
+// Y Axis
+var osc_y = ac.createOscillator();
+osc_y.connect(ac.destination);
+osc_y.type = "square";
+osc_y.start();
+
+
+
+
 document.addEventListener("mousemove", function(e){
-	console.log("X Is:" + e.clientX, "Y Is:" + e.clientY);
+	// console.log("X Is:" + e.clientX, "Y Is:" + e.clientY);
 	var x = e.clientX;
-	var y = e.clientY;
+	var y = h - e.clientY;
+
+	var x_perc = 200 + Math.floor(x/w*100)*10;
+	console.log(x_perc);
+	var y_perc = 200 + Math.floor(y/h*100)*10;
+	console.log(y_perc);
+
 	$("#pos").html("X Axis is: " + x + "<br>" + "Y Axis is: " + y);
-	changeTone(x);
+	
+	osc_x.frequency.value = x_perc;
+	osc_y.frequency.value = y_perc;
+	
 })
